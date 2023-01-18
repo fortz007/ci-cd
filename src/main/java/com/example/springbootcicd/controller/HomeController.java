8b68802a41c7cd8de49e763fe0a2e84bd12fc2e0
fortune <fortuneojih@yahoo.com>
2023-01-18 16:24:09 +0100

    First commit

---
+++ src/main/java/com/example/springbootcicd/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicd.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello and welcome to ci/cd tutorial";
+    }
+}
